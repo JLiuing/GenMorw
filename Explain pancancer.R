e1d7aa19d59c84f5ea64f569bf157476a978c36a
JLiu <39269577+JLiuing@users.noreply.github.com>
2024-09-24 22:24:28 +0800

    Update Explain pancancer.R

--- Explain pancancer.R
+++ Explain pancancer.R
@@ -1,4 +1,12 @@
+# Retrieval of contribution percentages of various cancer cohorts to pancancer prioritized genes
+# After completing the identification of driver genes across multiple cancer cohorts, GenMorw can 
+# integrate these cohorts into a pan-cancer prioritization. The program can analyze the contribution
+# composition of each gene in the prioritization, with the contributions derived collectively from
+# multiple cancers. By default, the program uses all cancer data available under the "Results/Data" 
+# directory for analysis. Users can also modify the "cancers" parameter to customize settings.
+
 cancers <- list.files("Results/Data")
+
 gene_sort <- list()
 for (cancer in cancers)
 {
@@ -30,7 +38,6 @@ names(final_sort) <- all_considered_genes
 final_sort <- sort(final_sort)
 final_symbol <- names(final_sort)
 
-# 计算贡献度
 cancer_contribution <- list()
 for (gene in all_considered_genes)
 {
